51f7f4e2f2a71932da7eead145e048c79f624c48
eternaleight <eternaleight0@gmail.com>
2022-06-26 04:19:37 +0900

    fix(style): responsive margin-x

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -16,7 +16,7 @@ const CartItem = ({ id, img, title, price, amount }: Items) => {
     <div className="flex items-center justify-center mb-4 font-noto">
       <div className="flex items-center">
         <img
-          className="mx-4 w-[200px] h-[120px] object-[-0.3px]"
+          className="mx-4 max-md:mx-2 w-[200px] h-[120px] object-[-0.3px]"
           src={img}
           alt={title}
         />
